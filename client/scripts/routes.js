b81b7c9842290f15e8585cbead7fc5f370fd6094
split750 <godefroy.lemerdy@gmail.com>
2016-01-19 00:07:50 +0100

    add saveas file + csv

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -82,3 +82,41 @@ Router.route('/todo', function () {
   
   this.render('taskList');
 });
+
+
+Router.route('/profile/csv', { where: 'server' })
+  .get(function () {
+    // GET /webhooks/stripe
+    var userData = Meteor.user();
+    console.log(userData);
+    var date = new Date();
+    
+    var csv = Papa.unparse({
+      fields: ["BEGIN", "VCARD"],
+      data: [
+        ["VERSION", "3.0"],
+        ["FN;CHARSET=UTF-8", userData.profile.firstName + ' ' + userData.profile.lastName],
+        ["N;CHARSET=UTF-8", userData.profile.lastName + ';' + userData.profile.firstName + ';;;'],
+        ["EMAIL;CHARSET=UTF-8;type=WORK,INTERNET", userData.profile.job.email],
+        ["TEL;TYPE=WORK,VOICE", userData.profile.job.tel],
+        ["LABEL;CHARSET=UTF-8;TYPE=WORK", "Work Address"],
+        ["ADR;CHARSET=UTF-8;TYPE=WORK", ';;' + userData.profile.job.address + ';' + userData.profile.job.city +';;' + userData.profile.job.postalCode + ';' + userData.profile.job.countryRegion],
+        ["TITLE;CHARSET=UTF-8:", userData.profile.job.title],
+        ["ORG;CHARSET=UTF-8", userData.profile.job.company],
+        ["URL;CHARSET=UTF-8", userData.profile.job.website],
+        ["X-SOCIALPROFILE;CHARSET=UTF-8;TYPE=twitter", userData.profile.social.twitter],
+        ["REV", date],
+        ["END", "VCARD"]
+      ]
+    }, {
+      quotes: false,
+      delimiter: ":",
+      newline: "\r\n"
+    });
+    
+    console.log(csv);
+    
+    var csvFile = new Blob(csv, {type: "text/vcard;charset=utf-8"});
+	saveAs(blob, userData.profile.firstName + ' ' + userData.profile.lastName + '.vcf');
+ 
+  })
